ce5681bc69b97ff50a39198686f344c6a6feda77
Mayra <mabaezj@gmail.com>
2025-05-28 15:54:33 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import PrivilegeIndexApp from './PrivilegeIndexApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PrivilegeIndexApp />
+  </React.StrictMode>
+);
